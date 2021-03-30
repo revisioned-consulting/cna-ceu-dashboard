3aca57a67995fd0116bba0b8be05ea64c745d0c7
George Bockari <bitwhys@gmail.com>
2021-03-29 21:30:34 -0700

    chore: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
